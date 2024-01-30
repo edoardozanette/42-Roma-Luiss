8e5d2c4496fccf14e8f9ba404c48aa99690dcc07
edoardozanette <101724056+edoardozanette@users.noreply.github.com>
2024-01-30 19:31:23 +0100

    itoa corretto

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,101 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 //#include "libft.h"
 
-int	length(int nb, int base)
+int ft_len(int n)
 {
-	int	count;
-
-	count = 0;
-	if (nb <= 0)
-		count++;
-	while (nb && count++)
-		nb /= base;
-	return (count);
-}*/
+    int i;
+    
+    i = 0;
+    if (n < 0)
+        i++;
+   while (n)
+    { 
+        n /= 10;
+        i++;
+        printf("bella\n");
+    }
+    return (i + 1);
+}
 
 char *ft_itoa(int n)
 {
-    int        len;
-    char       *res;
-    const char *digits = "0123456789";
+    int len;
+    int i;
+    char *res;
 
-    len = length(n, 10);
-    res = malloc(sizeof(char) * (len + 1));
+    len = ft_len(n);
+     printf("len: %i\n", len);
+    i = len;
+    res = malloc(len * sizeof(char));
     if (!res)
-    {
         return (0);
-    }
-    res[len] = 0;
-	if (n == 0)
-    {
-        res[0] = '0';
-    }
+    res[i - 1] = '\0';
     if (n < 0)
     {
         res[0] = '-';
+        n *= -1;
     }
-    while (n)
+    len -= 2;
+    while(n > 0)
     {
-        if (n > 0)
-            res[len--] = digits[n % 10];
-        else 
-            res[len--] = digits[n % 10 * -1];
+        res[len] = (n % 10) + '0';
+        printf("res: %c\n numero: %i\n\n", res[len - 1], (n % 10));
         n /= 10;
+        len--;
     }
     return (res);
 }
-/*
-int	lenght(int nb, int base)
-{
-	int	count;
-
-	count = 0;
-	if (nb <= 0)
-		++count;
-	while (nb && ++count)
-		nb /= base;
-	return (count);
-}
-
-char	*ft_itoa(int nb)
-{
-	int			len;
-	char		*str;
-	const char	*digits = "0123456789";
-
-	len = lenght(nb, 10);
-	str = malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (0);
-	str[len] = 0;
-	if (nb == 0)
-		str[0] = '0';
-	if (nb < 0)
-		str[0] = '-';
-	while (nb)
-	{
-		if (nb > 0)
-			str[--len] = digits[nb % 10];
-		else
-			str[--len] = digits[nb % 10 * -1];
-		nb /= 10;
-	}
-	return (str);
-}*/
 
 int main() {
-    int number = -12345;
+    int number = -3245;
     char *result = ft_itoa(number);
 
-    if (result != NULL) {
+    if (result != NULL) 
+    {
         printf("Stringa risultante: %s\n", result);
-
-        // Libera la memoria allocata per la stringa risultante
-        free(result);
-    } else {
-        printf("Errore durante l'allocazione di memoria.\n");
     }
-
     return 0;
 }
