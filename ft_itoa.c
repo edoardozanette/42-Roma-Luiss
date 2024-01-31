04aff6724da58b99815696e1dab7122aa9d79354
edoardozanette <101724056+edoardozanette@users.noreply.github.com>
2024-01-31 14:27:27 +0000

    correcting itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,11 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-//#include "libft.h"
+#include "libft.h"
 
-int ft_len(int n)
+size_t ft_len(int n)
 {
-    int i;
+    size_t i;
     
     i = 0;
     if (n < 0)
@@ -14,24 +11,39 @@ int ft_len(int n)
     { 
         n /= 10;
         i++;
-        printf("bella\n");
     }
     return (i + 1);
 }
 
+control_n(int n)
+{
+    if (n == 0)
+    {
+        res[0] = '0';
+        return (res);
+    }
+    if (n < 0)
+    {
+        res[0] = '-';
+    }
+}
 char *ft_itoa(int n)
 {
-    int len;
-    int i;
+    size_t len;
+    size_t i;
     char *res;
 
     len = ft_len(n);
-     printf("len: %i\n", len);
     i = len;
     res = malloc(len * sizeof(char));
     if (!res)
         return (0);
     res[i - 1] = '\0';
+    if (n == 0)
+    {
+        res = '0';
+        return (res);
+    }
     if (n < 0)
     {
         res[0] = '-';
@@ -41,20 +53,8 @@ char *ft_itoa(int n)
     while(n > 0)
     {
         res[len] = (n % 10) + '0';
-        printf("res: %c\n numero: %i\n\n", res[len - 1], (n % 10));
         n /= 10;
         len--;
     }
     return (res);
-}
-
-int main() {
-    int number = -3245;
-    char *result = ft_itoa(number);
-
-    if (result != NULL) 
-    {
-        printf("Stringa risultante: %s\n", result);
-    }
-    return 0;
 }
