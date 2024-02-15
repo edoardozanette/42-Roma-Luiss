8e4a04aad102f8c27d72b15b0581200ab3c51eee
edoardozanette <101724056+edoardozanette@users.noreply.github.com>
2024-02-15 17:53:02 +0100

    Update ft_printstr.c
    da completare il return

---
+++ ft_printf/ft_printstr.c
@@ -0,0 +1,14 @@
+int ft_putstr_fd(char *s, int fd)
+{
+    int i;
+
+    if (!s || !fd)
+        return;
+    i = 0;
+    while (s[i] != '\0')
+    {
+    write(fd, &s[i], 1);
+    i++;
+    }
+    
+}
